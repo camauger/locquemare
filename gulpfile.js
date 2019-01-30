ca0e5f3bcbed78713369266346f2836a384035b7
Christian Amauger <christian.amauger@gmail.com>
2019-01-29 19:36:41 -0500

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,141 @@
+"use strict";
+
+// Load plugins
+const autoprefixer = require("autoprefixer");
+const browsersync = require("browser-sync").create();
+//const cp = require("child_process");
+const cssnano = require("cssnano");
+//const del = require("del");
+//const eslint = require("gulp-eslint");
+const gulp = require("gulp");
+//const imagemin = require("gulp-imagemin");
+//const newer = require("gulp-newer");
+const plumber = require("gulp-plumber");
+const postcss = require("gulp-postcss");
+const rename = require("gulp-rename");
+const sass = require("gulp-sass");
+//const webpack = require("webpack");
+//const webpackconfig = require("./webpack.config.js");
+//const webpackstream = require("webpack-stream");
+
+// BrowserSync
+function browserSync(done) {
+  browsersync.init({
+    server: {
+      baseDir: "./app"
+    },
+    port: 3000
+  });
+  done();
+}
+
+// BrowserSync Reload
+function browserSyncReload(done) {
+  browsersync.reload();
+  done();
+}
+
+// Clean assets
+function clean() {
+  return del(["./app/assets/"]);
+}
+
+// Optimize Images
+function images() {
+  return gulp
+    .src("./assets/images/**/*")
+    .pipe(newer("./app/assets/images"))
+    .pipe(
+      imagemin([
+        imagemin.gifsicle({ interlaced: true }),
+        imagemin.jpegtran({ progressive: true }),
+        imagemin.optipng({ optimizationLevel: 5 }),
+        imagemin.svgo({
+          plugins: [
+            {
+              removeViewBox: false,
+              collapseGroups: true
+            }
+          ]
+        })
+      ])
+    )
+    .pipe(gulp.dest("./app/assets/images"));
+}
+
+// CSS task
+function css() {
+  return gulp
+    .src("./app/assets/scss/**/*.scss")
+    .pipe(plumber())
+    .pipe(sass({ outputStyle: "expanded" }))
+    .pipe(gulp.dest("./app/assets/css/"))
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(postcss([autoprefixer(), cssnano()]))
+    .pipe(gulp.dest("./app/assets/css/"))
+    .pipe(browsersync.stream());
+}
+
+// Lint scripts
+function scriptsLint() {
+  return gulp
+    .src(["./assets/js/**/*", "./gulpfile.js"])
+    .pipe(plumber())
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
+}
+
+// Transpile, concatenate and minify scripts
+function scripts() {
+  return (
+    gulp
+      .src(["./assets/js/**/*"])
+      .pipe(plumber())
+      .pipe(webpackstream(webpackconfig, webpack))
+      // folder only, filename is specified in webpack config
+      .pipe(gulp.dest("./app/assets/js/"))
+      .pipe(browsersync.stream())
+  );
+}
+
+// Jekyll
+function jekyll() {
+  return cp.spawn("bundle", ["exec", "jekyll", "build"], { stdio: "inherit" });
+}
+
+// Watch files
+function watchFiles() {
+  gulp.watch("./app/assets/scss/**/*", css);
+  gulp.watch("./app/assets/js/**/*", gulp.series(scriptsLint, scripts));
+  gulp.watch(
+    [
+      "./_includes/**/*",
+      "./_layouts/**/*",
+      "./_pages/**/*",
+      "./_posts/**/*",
+      "./_projects/**/*"
+    ],
+    gulp.series(jekyll, browserSyncReload)
+  );
+  gulp.watch("./assets/images/**/*", images);
+}
+
+
+
+
+
+// define complex tasks
+const js = gulp.series(scriptsLint, scripts);
+const build = gulp.series(clean, gulp.parallel(css, images, jekyll, js));
+const watch = gulp.parallel(watchFiles, browserSync);
+
+// export tasks
+exports.images = images;
+exports.css = css;
+exports.js = js;
+exports.jekyll = jekyll;
+exports.clean = clean;
+exports.build = build;
+exports.watch = watch;
+exports.default = build;
---
+++ images.md
@@ -0,0 +1,48 @@
+Image #	Artiste	Source 1	Source 2
+1	Eddie Mendoza	https://geekquestioner.com/content/1846-lowriders-eddie-mendoza
+2	Eddie Mendoza	https://www.deviantart.com/eddie-mendoza/art/The-Meditation-Garden-775650874
+3	Eddie Mendoza	https://www.deviantart.com/eddie-mendoza/art/Bloated-Whale-House-760073279
+4	Eddie Mendoza	https://www.deviantart.com/eddie-mendoza/art/Pit-Stop-755059339
+5	Eddie Mendoza	https://www.deviantart.com/eddie-mendoza/art/Zygote-Market-750417713
+6	Eddie Mendoza	https://www.deviantart.com/eddie-mendoza/art/Mining-Town-750417281
+7	Eddie Mendoza	https://www.deviantart.com/eddie-mendoza/art/La-Capital-740164950
+8	Eddie Mendoza	https://www.deviantart.com/eddie-mendoza/art/Safehouse-735032708
+9	Eddie Mendoza	https://www.deviantart.com/eddie-mendoza/art/Settlement-696156277
+10	Eddie Mendoza	https://www.deviantart.com/eddie-mendoza/art/Ruined-Empire-287780570
+11		https://www.deviantart.com/dominiquevvelsen/art/X2-773024267
+12		https://www.deviantart.com/thomaswievegg/art/Taking-The-Dog-For-a-Walk-766028587
+13		https://www.deviantart.com/raddick11/art/the-end-is-coming-572176631
+14		https://www.deviantart.com/superninjanub/art/All-Done-679552728
+15		https://www.deviantart.com/jonasdero/art/Dodge-This-452672142
+16		https://www.deviantart.com/jeremypaillotin/art/Sense-Collective-Vertical-farms-568813833
+17		https://www.deviantart.com/tryingtofly/art/The-town-called-scyphio-598590983
+18		https://www.deviantart.com/shue13/art/The-Capital-Of-Hope-721198944
+19		Photo by Alex Knight on Unsplash
+20		Photo by Zoltan Tasi on Unsplash
+21		Photo by Josh Hild on Unsplash
+22		Photo by Taton Moïse on Unsplash
+23		Photo by Ant Rozetsky on Unsplash
+24		Photo by Ant Rozetsky on Unsplash
+25		Photo by Isis França on Unsplash
+26		Photo by Anna Jiménez Calaf on Unsplash
+27		Photo by Jakub Skafiriak on Unsplash
+28		Photo by Hanny Naibaho on Unsplash
+29		Photo by Linus Sandvide on Unsplash
+30		Photo by Witch Kiki on Unsplash
+31		Photo by Clarisse Meyer on Unsplash
+32		Photo by Nico on Unsplash
+33		Photo by Erik Eastman on Unsplash
+34		Photo by Garett Mizunaka on Unsplash
+35		Photo by Hasan Almasi on Unsplash
+36		Photo by Jack B on Unsplash
+37		Photo by Warren Wong on Unsplash
+38		Photo by Asif Aman on Unsplash
+39	https://geekquestioner.com/content/1876-adventure-lies-within-armieri		
+40	https://geekquestioner.com/content/1875-extra-credit-andreas-rocha		
+41	https://www.deviantart.com/panjoool/art/Practice-779011180		
+42	https://www.deviantart.com/panjoool/art/Pen-Dragon-781630904		
+43	https://www.deviantart.com/kuldarleement/art/Ezonova-3-572753102		
+
+	https://www.pinterest.fr/davidmcguire56/post-apocalyptic-portrait/?lp=true		
+
+	https://geekquestioner.com/content/1040-stopping-for-breath-vincent-lefevre?rq=cyberpunk		
